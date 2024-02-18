9d3de55c7c567c3d08de195d7db7e22537cd768c
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-02-18 16:46:27 +0800

    Update loadstring.lua

--- test/loadstring.lua
+++ test/loadstring.lua
@@ -1,2 +1,26 @@
 if game.PlaceId ~= 9431156611 then return end
+getgenv().disableBarriers = true
+getgenv().hazardCollision = true
+
+getgenv().itemVacEnabled = true
+getgenv().itemVacHidePlayer = true
+getgenv().itemVacWaitForBus = false -- This will override itemVacHidePlayer
+getgenv().itemVacAllowBruteForce = true
+
+getgenv().bombBus = true
+getgenv().permaTruePower = true -- Activates when you have 2 or more True Powers
+getgenv().usePermaItems = true
+
+getgenv().instantBusJump = true
+getgenv().teleportToGroundOnBusJump = true
+
+getgenv().safetyHeal = true
+getgenv().healthLow = 30
+getgenv().healthOk = 80
+
+getgenv().killAll = true
+getgenv().killAllStudsPerSecond = 420
+
+if getgenv().SRCheatConfigured then return end
+getgenv().SRCheatConfigured = true
 loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Voxul/RobloxScripts/main/test/SlapRoyaleTest.lua'))()
