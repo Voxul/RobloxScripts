47bc45bae1b73421e67e8992fc75a3627c0180b3
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-02-19 18:03:26 +0800

    Update SlapRoyaleTest.lua

--- test/SlapRoyaleTest.lua
+++ test/SlapRoyaleTest.lua
@@ -136,7 +136,7 @@ if getgenv().breakGame then
 	for i,v in workspace:GetDescendants() do
 		if v:IsA("BasePart") and not v:IsDescendantOf(LocalPlr.Character) then
 			Events.Item:FireServer(v)
-			if i%3 == 0 then
+			if i%5 == 0 then
 				task.wait()
 			end
 		end
