132008094325ff525b7d457b56784abe546ef3aa
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-02-21 18:09:14 +0800

    Update loadstring.lua

--- SlapBattles/SlappleAutoFarm/loadstring.lua
+++ SlapBattles/SlappleAutoFarm/loadstring.lua
@@ -1,3 +1,4 @@
 local getgenv = getgenv or getfenv
 getgenv().targetSlaps = 1000
 
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Voxul/RobloxScripts/3f7297f3d2cfc0037e5953301259f38d869ec0a5/SlapBattles/SlappleAutoFarm/Slapple%20Farm.lua'))()
