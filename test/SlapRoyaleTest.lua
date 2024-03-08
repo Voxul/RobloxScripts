4f8136628ef2cede3a9a397207ed09b569d6de31
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-03-08 18:55:26 +0800

    one more thing

--- test/SlapRoyaleTest.lua
+++ test/SlapRoyaleTest.lua
@@ -399,6 +399,10 @@ RunService.Heartbeat:Connect(function(dT)
 			lastPositions[char].posBuffer = char.HumanoidRootPart.Position
 		end
 	end
+	
+	if HumanoidRootPart.Position.Y < -180 then
+		pivotModelTo(Character, HumanoidRootPart.CFrame - Vector3.new(0, HumanoidRootPart.Position.Y + 100, 0), true)
+	end
 end)
 
 local lOSParams = RaycastParams.new()
@@ -423,7 +427,7 @@ while task.wait() and not Character:FindFirstChild("Dead") do
 			pivotModelTo(
 				Character, 
 				CFrame.new(
-					HumanoidRootPart.Position:Lerp(HumanoidRootPart.Position*Vect3_XZ, (studsPerSecond/originalY * lastDelta))
+					HumanoidRootPart.Position:Lerp(HumanoidRootPart.Position*Vect3_XZ, studsPerSecond/originalY * lastDelta)
 				)*CFrame.Angles(math.rad(180), 0, 0),
 				true
 			)
@@ -500,15 +504,6 @@ while task.wait() and not Character:FindFirstChild("Dead") do
 
 		moveToTick = os.clock()
 		task.wait()
-	end
-
-	if HumanoidRootPart.Position.Y < -180 then
-		pivotModelTo(Character, HumanoidRootPart.CFrame - Vector3.new(0, HumanoidRootPart.Position.Y + 100, 0), true)
-		-- attempt countermeasures against other exploiters
-		table.insert(ignores, target)
-		task.delay(0.8, function()
-			table.remove(ignores, table.find(ignores, target))
-		end)
 	end
 
 	target, distance = getClosestHittableCharacter(HumanoidRootPart.Position)
