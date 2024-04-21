0e96080800c70d1c8fc966a5f5b8c9fccf0c51e5
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-04-21 19:39:38 +0800

    replace loadfile with loadstring(readfile) for codex compatibility

--- SlapBattles/SlappleAutoFarm/loadstring.lua
+++ SlapBattles/SlappleAutoFarm/loadstring.lua
@@ -1,2 +1,2 @@
 getgenv().TargetSlaps = 1000 -- The amount of slaps to farm to
-writefile("Voxul_SlappleFarm.txt",game:HttpGet("https://github.com/Voxul/RobloxScripts/raw/main/SlapBattles/SlappleAutoFarm/Slapple%20Farm.lua"));loadfile("Voxul_SlappleFarm.txt")()
+writefile("Voxul_SlappleFarm.txt",game:HttpGet("https://github.com/Voxul/RobloxScripts/raw/main/SlapBattles/SlappleAutoFarm/Slapple%20Farm.lua"));loadstring(readfile("Voxul_SlappleFarm.txt"))()
