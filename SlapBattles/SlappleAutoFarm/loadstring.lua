600a2cf2a782a8c9b83ef94522c7736ce891b1ca
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-04-21 18:47:43 +0800

    Update loadstring.lua

--- SlapBattles/SlappleAutoFarm/loadstring.lua
+++ SlapBattles/SlappleAutoFarm/loadstring.lua
@@ -1,5 +1,2 @@
-local getgenv = getgenv or getfenv
-getgenv().targetSlaps = 1000
-if not game:IsLoaded() then game.Loaded:Wait() end
-if not game.Players.LocalPlayer:FindFirstChild("leaderstats") or not game.Players.LocalPlayer.leaderstats:FindFirstChild("Slaps") or game.Players.LocalPlayer.leaderstats.Slaps.Value >= getgenv().targetSlaps then return end
-loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Voxul/RobloxScripts/3f7297f3d2cfc0037e5953301259f38d869ec0a5/SlapBattles/SlappleAutoFarm/Slapple%20Farm.lua'))()
+getgenv().TargetSlaps = 1000 -- The amount of slaps to farm to
+loadstring(game:HttpGet("https://github.com/Voxul/RobloxScripts/raw/main/SlapBattles/SlappleAutoFarm/Slapple%20Farm.lua"))()
