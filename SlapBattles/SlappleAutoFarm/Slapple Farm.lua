e5b2eb4a41d82bf4870ed3102354b859f234ea79
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-04-21 18:46:23 +0800

    Update Slapple Farm.lua

--- SlapBattles/SlappleAutoFarm/Slapple Farm.lua
+++ SlapBattles/SlappleAutoFarm/Slapple Farm.lua
@@ -1,40 +1,41 @@
-local getgenv = getgenv or getfenv
-if not getgenv().targetSlaps then getgenv().targetSlaps = 1000 end
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local HRM = Character:WaitForChild("HRM")
 
-if not game:IsLoaded() then game.Loaded:Wait() end
-local LPLR = game:GetService("Players").LocalPlayer
-local char = LPLR.Character or LPLR.CharacterAdded:Wait()
-local hrm = char:WaitForChild("HumanoidRootPart")
-local tp1 = workspace.Lobby.Teleport1
-
-local function touchPart(part, otherPart, state)
-	if type(firetouchinterest)=="function" then
-		firetouchinterest(part,otherPart,state or 0)
-		if not state then firetouchinterest(part,otherPart,1) end
-	end
-	part.CFrame = otherPart.CFrame
+local function touchPart(p:BasePart,oP:BasePart,s:boolean?,d:number?)
+	firetouchinterest(p,oP,s or 0)
+	if d then task.wait(d) end
+	if not s then firetouchinterest(p,oP,1) end
 end
 
-while not char:FindFirstChild("entered") do
-	touchPart(tp1,hrm)
+while not Character:FindFirstChild("entered") do
+	touchPart(workspace:WaitForChild("Lobby"):WaitForChild("Teleport1"), HRM)
 	task.wait()
 end
 
-for _,v in workspace.Arena.island5.Slapples:GetDescendants() do
-	if v.Name=="Glove" and v:FindFirstChild("TouchInterest") then
-		touchPart(v,hrm)
+for _,v:Model in workspace.Arena.island5.Slapples:GetChildren() do
+	if v:FindFirstChild("Glove") then
+		touchPart(v.Glove,HRM)
 	end
 end
 
-local serverList = {}
-for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
-	if v.playing and type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
-		table.insert(serverList,v.id)
+local serverList, completed = {}, false
+-- perform two HTTP GET at same time
+task.spawn(function()
+	for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/6403373529/servers/Public?sortOrder=Asc&limit=100")).data) do
+		if v.playing and type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+			table.insert(serverList,v.id)
+		end
 	end
-end
+	completed = true
+end)
 
-if #serverList > 0 then
+queue_on_teleport("getgenv().TargetSlaps = "..getgenv().TargetSlaps..";"..game:HttpGet(""))
+
+while not completed do task.wait() end
+
+if serverList[1] then
 	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, serverList[math.random(1, #serverList)])
 else
-	error("No servers found")
+	warn("No servers found")
 end
