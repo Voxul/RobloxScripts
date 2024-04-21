4a29a50f1366e6381258f21ae95fe2dc3689d1da
VoxulRBLX <160318120+Voxul@users.noreply.github.com>
2024-04-21 18:55:37 +0800

    Update loadstring.lua

--- SlapBattles/SlappleAutoFarm/loadstring.lua
+++ SlapBattles/SlappleAutoFarm/loadstring.lua
@@ -1,2 +1,2 @@
 getgenv().TargetSlaps = 1000 -- The amount of slaps to farm to
-loadstring(game:HttpGet("https://github.com/Voxul/RobloxScripts/raw/main/SlapBattles/SlappleAutoFarm/Slapple%20Farm.lua"))()
+writefile("Voxul_SlappleFarm",game:HttpGet("https://github.com/Voxul/RobloxScripts/raw/main/SlapBattles/SlappleAutoFarm/Slapple%20Farm.lua"));dofile("Voxul_SlappleFarm")
